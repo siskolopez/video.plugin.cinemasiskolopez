d303ec008aed6525f04e1152b3be49b9c8b72b25
siskolopez <siskolopez@users.noreply.github.com>
2015-10-21 01:49:21 +0200

    Update default.py

--- default.py
+++ default.py
@@ -37,7 +37,7 @@ if REMOTE_DBG:
         sys.exit(1)  
 
 
-addon = xbmcaddon.Addon('plugin.video.CINEMASISKOLOPEZ')
+addon = xbmcaddon.Addon('video.plugin.cinemasiskolopez')
 addon_version = addon.getAddonInfo('version')
 profile = xbmc.translatePath(addon.getAddonInfo('profile').decode('utf-8'))
 home = xbmc.translatePath(addon.getAddonInfo('path').decode('utf-8'))
